b48fe1437cb5620cea6916c32d394d3c3d190e47
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-05-25 00:05:59 -0400

    versiton 0.0.1-Initial Commit

---
+++ COMP123-S2016-CKawakami-300821245-Lab2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*This is driver class
+ * Author:Chinatsu Kawakami
+ * Date:May 24th, 2016
+ * Date Modified: May 24th, 2016
+ * Student Id: 300821245
+ * Description: This program calls instance variables from person class 
+ * and print SayHello Method and ShowAge Method
+ * Version : 0.0.1-Initialize varsion
+ */
+namespace COMP123_S2016_CKawakami_300821245_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
