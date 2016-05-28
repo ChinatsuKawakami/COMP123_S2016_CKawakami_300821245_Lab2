7847c075184d801492df0c1cb297c8c210191818
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-05-27 20:38:34 -0400

    added public Age property , methods and constructor. Added instance objects into driver class

--- COMP123-S2016-CKawakami-300821245-Lab2/Person.cs
+++ COMP123-S2016-CKawakami-300821245-Lab2/Person.cs
@@ -22,8 +22,9 @@ namespace COMP123_S2016_CKawakami_300821245_Lab2
          //PUBLIC INSTANCE VARIABLES++++++++++++++++++++++++++++++++++
 
         /**
+         * <summary>
          * This is a property for our _name field
-         * 
+         * </summary>
          * @property {string} Name
          */
         public string Name
@@ -40,41 +41,129 @@ namespace COMP123_S2016_CKawakami_300821245_Lab2
     }//close Name property
 
     /**
+     *<summary>
+     *This is public property for our _age field
+     * </summary>
+     * @property {int} Age
+     */
+        public int Age
+        {
+            get
+            {
+                return this._age;
+            }
+            set
+            {
+                this._age = value;
+            }
+        }//close public Age property
+
+   //CONSTRUCTOR++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    /**
+     * <summary>
      * This is the default empty constructor for The Person Class
-     * 
+     * </summary>
      * @constructor Person
      * 
      */
 public Person()
  {
      
-         this._name = "Tom";
-         this._age = 47;
-    
+         //this._name = "Tom";
+        // this._age = 47;
+     this.initialize();
 }
+
+/**
+ * <summary>
+ * This is constructor that takes name as a parameter and passes 
+ * It to the _name private instance variable
+ * </summary>
+ * @constructor Person
+ * @param {string} name
+ */
+public Person(string name)
+{
+    this.Name = name;
+    this.Age = 0;
+   // this.initialize();
+
+}
+
+/**
+ * <summary>
+ * This is the constructor that takes age as a parameter and passes 
+ * It to the _age private instance variable
+ * </summary>
+ * @constructor Person
+ * @param {int} age
+ */
+
+public Person(int age)
+{
+    this.Name = "UNKNOWN NAME!!";
+    this.Age = age;
+ //   this.initialize();
+   
+}
+
+
+/**
+ * <summary>
+ * This is a constructor that takes both name and age as parameter
+ * it assigns the locak name value to the name property
+ * it assigns the local age value to the age property
+ * </summary>
+ * 
+ */
 
 public Person(string name,int age)
 {
     this._name = name;
     this._age = age;
+   // this.initialize();
 }
 
-//This is my SayHello method 
-//method SayHello
-//this method has a return type which is void(does not return a value)
+/**
+ * <summary>
+ * This is a constructor with parameter less 
+ *
+ * </summary>
+ * 
+ * @private 
+ * @method _initialize
+ * @return {void}
+ */
 
-public void SayHello()
+private void initialize()
+{
+    this.Name = "UNKNOWN NAME!";
+    this.Age = 0;
+
+}
+
+/**
+ *<summary>
+ *This method outputs the_name value with "says hello!" to console
+ *</summary>
+ *@method SayHello
+ *return {void}
+*/
+public void SaysHello()
  {
-     Console.WriteLine("{0} says hello", this._name);
+     Console.WriteLine("{0} says hello!", this.Name);
   }
-
-//This is my SayHello method 
-//method ShowAge
-//this method has a return type which is void(does not return a value)
-
+/**
+ * <summary>
+ *This method outputs the _name value and _age value in the following
+ *format: "{0} is {1} years old", this.Name, this.Age
+ * </summary>
+ * @method ShowAge
+ * @return {void}
+*/
 public void ShowAge()
 {
-    Console.WriteLine("{0} is {1} years old", this._name, this._age);
+    Console.WriteLine("{0} is {1} years old", this.Name, this.Age);
 }
     
     }//close Person Class
--- COMP123-S2016-CKawakami-300821245-Lab2/Program.cs
+++ COMP123-S2016-CKawakami-300821245-Lab2/Program.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 /*This is driver class
  * Author:Chinatsu Kawakami
  * Date:May 24th, 2016
- * Date Modified: May 25th, 2016
+ * Date Modified: May 27th, 2016
  * Student Id: 300821245
  * Description: This program calls instance variables from person class 
  * and print SayHello Method and ShowAge Method
- * Version : 0.0.6-Updated Person Class
+ * Version : 0.0.7-added public Age property , methods and constructor. Added instance objects into driver class
  */
 namespace COMP123_S2016_CKawakami_300821245_Lab2
 {
@@ -32,12 +32,67 @@ namespace COMP123_S2016_CKawakami_300821245_Lab2
             //create a new instance of the Person class
             Person person1 = new Person();
             Person person2 = new Person("Mark",27);
-            // call the Sayhello and ShowAge method
-            person1.SayHello();
-            person1.ShowAge();
+            Person person3 = new Person("Jean");
+            Person person4 = new Person(50);
+            Person person5 = new Person();
+            person5.Name = "Jake";
+            person5.Age = 30;
+            Person person6 = new Person();
+            person6.Name = "Maria";
+            Person person7 = new Person();
+            person7.Age = 60;
+            Person person8 = new Person("Ashely");
+            person8.Age = 19;
+            Person person9 = new Person(29);
+            person9.Name = "Bob";
 
-            person2.SayHello();
+            // call the Sayshello and ShowAge method(get)
+            person1.SaysHello();
+            person1.ShowAge();
+            Console.WriteLine();
+            Console.WriteLine("=====================================================");
+
+            person2.SaysHello();
             person2.ShowAge();
+            Console.WriteLine();
+            Console.WriteLine("=====================================================");
+
+            person3.SaysHello();
+            person3.ShowAge();
+            Console.WriteLine();
+            Console.WriteLine("=====================================================");
+
+            person4.SaysHello();
+            person4.ShowAge();
+            Console.WriteLine();
+            Console.WriteLine("=====================================================");
+
+            // call the Sayshello and ShowAge method(set)
+            person5.SaysHello();
+            person5.ShowAge();
+            Console.WriteLine();
+            Console.WriteLine("=====================================================");
+
+            person6.SaysHello();
+            person6.ShowAge();
+            Console.WriteLine();
+            Console.WriteLine("=====================================================");
+
+            person7.SaysHello();
+            person7.ShowAge();
+            Console.WriteLine();
+            Console.WriteLine("=====================================================");
+
+            // call the Sayshello and ShowAge method(set & get)
+            person8.SaysHello();
+            person8.ShowAge();
+            Console.WriteLine();
+            Console.WriteLine("=====================================================");
+
+            person9.SaysHello();
+            person9.ShowAge();
+            Console.WriteLine();
+            Console.WriteLine("=====================================================");
         }
     }
 }
